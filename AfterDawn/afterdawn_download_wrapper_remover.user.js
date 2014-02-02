65f67947d0a38d54a63aea4e6821b74d8e579e89
Bruno Barbieri <brunorex@gmail.com>
2014-02-02 00:52:53 -0200

    "AfterDawn download wrapper remover" converted to pure JavaScript

--- AfterDawn/afterdawn_download_wrapper_remover.user.js
+++ AfterDawn/afterdawn_download_wrapper_remover.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           AfterDawn download wrapper remover
-// @version        0.4
+// @version        0.5
 // @author         Bruno Barbieri
 // @description    Rewrites the download link to remove the download wrapper in AfterDawn (i.e. "Safe Download" button)
 // @include        http://afterdawn.*/*
@@ -22,34 +22,33 @@
  * v0.3: Use alternate method for loading jQuery, now works with Chrome as well. Credits to Erik Vold (http://r.evold.ca/jquery4us).
  *       Use domain wildcards that work in Chrome as well, and add the Finnish domain.
  * v0.4: Added update URL and updated JQuery version
+ * v0.5: Pure JavaScript implementation (http://youmightnotneedjquery.com)
  *
  */
- 
-// A function that loads jQuery and calls a callback function when jQuery has finished loading
-function addJQuery(callback) {
-  var script = document.createElement("script");
-  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js");
-  script.addEventListener('load', function() {
-    var script = document.createElement("script");
-    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
-    document.body.appendChild(script);
-  }, false);
-  document.body.appendChild(script);
-}
+
 
 function main() {
     // Remove extra text from button (e.g. "Safe Download")
-    jQ("#download-main-button").html((jQ("#download-main-button").html().replace("<b>" + jQ('#download-main-button > b').html() + "</b>","")));
+    downBtn = document.querySelectorAll('#download-main-button')[0];
+    safeDownHtml = document.querySelectorAll('#download-main-button > b')[0].innerHTML;
+    downBtnHtml = downBtn.innerHTML.replace('<b>' + safeDownHtml + '</b>', '');
+    downBtn.innerHTML = downBtnHtml;
 
     // Remove extra style from button
-    jQ('#download-main-button').removeClass('extra-label');
+    className = 'extra-label';
+    if (downBtn.classList) {
+        downBtn.classList.remove(className)
+    } else {
+        downBtn.className = downBtn.className.replace(new RegExp('(^| )' + className.split(' ').join('|') + '( |$)', 'gi'), ' ');
+    }
 
     // Remove hidden input
-    jQ('input[name="installer"]').remove();
+    hiddenInput = document.querySelectorAll('input[name="installer"]')[0];
+    hiddenInput.parentNode.removeChild(hiddenInput);
 
     // Replace installer option in button URL
-    jQ('#download-main-button').attr('href', jQ('#download-main-button').attr('href').replace(/installer=[\d]+/, 'installer=0'));
+    downBtnUrl = downBtn.getAttribute('href').replace(/installer=[\d]+/, 'installer=0');
+    downBtn.setAttribute('href', downBtnUrl)
 }
 
-// Load jQuery and execute the main function
-addJQuery(main);
+main();
